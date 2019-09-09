5b5fdef83e96354cb8323ad16ffd7c8ebf7b4e47
RishabhArya <rishabharya2799@gmail.com>
2019-09-10 04:47:58 +0530

    two more problems solved

---
+++ Problem Solving/Picking_Numbers.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+int pickingNumbers(vector<int> a) {
+int max_value = 1,differ ;
+vector<int> arr;
+for(int i=0 ;i<a.size();i++){
+    for(int j=0;j<a.size();j++){
+        if(i != j){
+            differ = a[i]-a[j];
+            if((differ==0) || (differ==1)){
+                max_value++;
+            }
+        }
+    }
+    arr.push_back(max_value);
+    max_value =1;
+}
+max_value = arr[0];
+for(int k=0;k<arr.size();k++){
+    if(max_value<arr[k])
+    {
+        max_value=arr[k];
+    }
+}
+  return max_value;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = pickingNumbers(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
+
